331e4a98597862d66539fac6c9fa9d005142ea9a
Fajar Julyana <fajarjulyana1@gmail.com>
2023-10-29 17:41:18 +0700

    app.js LICENSE node_modules package.json package-lock.json public README.md server views added

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 //Routes
 router.get('',(req, res) => {
-    res.send("Hello World")
+    const locals = {
+        title: "Nodejs Blog",
+        description: "Simple Blog created with Nodejs, Express & MongoDb."
+    }
+    res.render('index', { locals });
 });
 
+router.get('/about',(req, res) => {
+    res.render('about');
+});
 
 module.exports = router;
